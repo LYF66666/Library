19966a4d69b9cb673a503015da57a39d3ba755f0
LYF66666 <924483784@qq.com>
2016-11-17 20:51:22 +0800

    6
    6

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,11 +11,11 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Timers.Timer t = new System.Timers.Timer(1000);   //实例化Timer类，设置间隔时间为10000毫秒； 
+            System.Timers.Timer t = new System.Timers.Timer(10000);   //实例化Timer类，设置间隔时间为10000毫秒； 
             t.Elapsed += new System.Timers.ElapsedEventHandler(ontime); //到达时间的时候执行事件；   
             t.AutoReset = true;   //设置是执行一次（false）还是一直执行(true)；   
             t.Enabled = true;     //是否执行System.Timers.Timer.Elapsed事件； 
-            System.Timers.Timer T = new System.Timers.Timer(1000);
+            System.Timers.Timer T = new System.Timers.Timer(10000);
             T.Elapsed += new System.Timers.ElapsedEventHandler(getfine);    
             T.AutoReset = true; 
             T.Enabled = true;
